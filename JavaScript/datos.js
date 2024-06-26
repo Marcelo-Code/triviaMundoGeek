e11959f5c8fab08e100e5824f91e6d058c06301e
Marcelo Feltes <feltesmarcelo@gmail.com>
2024-06-26 13:27:32 -0300

    1.0

--- JavaScript/datos.js
+++ JavaScript/datos.js
@@ -1,113 +1,114 @@
-async function crearRegistro() {
-    let confirmar = false;
-    let jugador = {};
-    let minutos = 0;
-    let segundos = 0;
+// Variables donde se guardan la url y la API key del repositorio
+
+const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
+const apiKey = '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK';
+let dataJson = [];
+
+
+async function apiLeerRegistros() {
     try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
         const response = await fetch(apiUrl, {
             headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
+                'X-Master-Key': apiKey
             }
         });
         if (!response.ok) {
             throw new Error('Error al obtener los datos');
         }
-        const data = await response.json();
+        const datos = await response.json();
 
-        confirmar = confirm("¿Crear nuevo registro?");
-        if (confirmar) {
-            jugador.nombre = prompt("Ingresar nuevo nombre: ");
-            jugador.respuestasCorrectas = Number(prompt("Ingresar cantidad de respuestas correctas: "));
-            minutos = parseInt(prompt("Ingresar minutos: "));
-            segundos = parseFloat(prompt("Ingresar segundos: "));
-            segundos = Number(segundos.toFixed(3));
-            jugador.tiempo = minutos * 60000 + segundos * 1000;
-            jugador.posicion = 0;
+        dataJson = datos;
 
-            if((data.record.length == 1) && (data.record[0].nombre == "..."))
-                    {
-                        data.record[0].nombre = jugador.nombre;
-                        data.record[0].respuestasCorrectas = jugador.respuestasCorrectas;
-                        data.record[0].tiempo = jugador.tiempo;
-                    }
-            else data.record.push(jugador);
-
-            console.log(data.record);
-
-            guardarDatos(data);
-
-            //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
-
-            setTimeout(() => {
-                cargarRegistros()
-            }, 500);
-        }
     } catch (error) {
         console.error('Error al cargar los datos:', error);
     }
+}
+
+
+async function crearRegistro() {
+    let confirmar = false;
+    let jugador = {};
+    let minutos = 0;
+    let segundos = 0;
+
+    await apiLeerRegistros();
+
+    confirmar = confirm("¿Crear nuevo registro?");
+    if (confirmar) {
+        jugador.nombre = prompt("Ingresar nuevo nombre: ");
+        jugador.respuestasCorrectas = Number(prompt("Ingresar cantidad de respuestas correctas: "));
+        minutos = parseInt(prompt("Ingresar minutos: "));
+        segundos = parseFloat(prompt("Ingresar segundos: "));
+        segundos = Number(segundos.toFixed(3));
+        jugador.tiempo = minutos * 60000 + segundos * 1000;
+        jugador.posicion = 0;
+
+        //Si lo que existe es el registro vacío, se reemplazan los valores del mismo con los ingresados
+
+        if ((dataJson.record.length == 1) && (dataJson.record[0].nombre == "...")) {
+            dataJson.record[0].nombre = jugador.nombre;
+            dataJson.record[0].respuestasCorrectas = jugador.respuestasCorrectas;
+            dataJson.record[0].tiempo = jugador.tiempo;
+        } else dataJson.record.push(jugador);
+
+        guardarDatos(dataJson);
+
+        //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
+
+        setTimeout(() => {
+            cargarRegistros()
+        }, 500);
+    }
+
 
 }
 
 // Función para cargar datos del servidor
 
 async function cargarRegistros() {
-    try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-        const response = await fetch(apiUrl, {
-            headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
-            }
+
+    await apiLeerRegistros();
+
+    const datosPosicion = document.getElementById("datosPosicion");
+    const datosNombre = document.getElementById("datosNombre");
+    const datosCorrectas = document.getElementById("datosCorrectas");
+    const datosTiempo = document.getElementById("datosTiempo");
+
+    let mensajePosicion = "";
+    let mensajeNombre = "";
+    let mensajeCorrectas = "";
+    let mensajeTiempo = "";
+
+    //Si lo que existe es el registro vacío, el mismo no se muestra
+
+    if ((dataJson.record.length == 1) && (dataJson.record[0].nombre == "...")) alert("no hay registro de jugadores");
+    else {
+        dataJson.record.forEach(element => {
+            mensajePosicion += `<div>${element.posicion}</div>`;
+            mensajeNombre += `<div>${element.nombre}</div>`;
+            mensajeCorrectas += `<div>${element.respuestasCorrectas}</div>`;
+            let minutos = Math.floor((element.tiempo / 60000));
+            let segundos = Math.floor((element.tiempo - minutos * 60000) / 1000);
+            let milisegundos = element.tiempo - minutos * 60000 - segundos * 1000;
+            minutos = minutos.toString().padStart(2, '0');
+            segundos = segundos.toString().padStart(2, '0');
+            milisegundos = milisegundos.toString().padStart(3, '0');
+            mensajeTiempo += `<div>${minutos}' ${segundos}.${milisegundos}"</div>`;
         });
-        if (!response.ok) {
-            throw new Error('Error al obtener los datos');
-        }
-        const data = await response.json();
-
-        // console.log(data.record.length);
-
-        const datosPosicion = document.getElementById("datosPosicion");
-        const datosNombre = document.getElementById("datosNombre");
-        const datosCorrectas = document.getElementById("datosCorrectas");
-        const datosTiempo = document.getElementById("datosTiempo");
-
-        let mensajePosicion = "";
-        let mensajeNombre = "";
-        let mensajeCorrectas = "";
-        let mensajeTiempo = "";
-
-        if((data.record.length == 1) && (data.record[0].nombre == "...")) alert("no hay registro de jugadores");
-        else
-            {
-                data.record.forEach(element => {
-                    mensajePosicion += `<div>${element.posicion}</div>`;
-                    mensajeNombre += `<div>${element.nombre}</div>`;
-                    mensajeCorrectas += `<div>${element.respuestasCorrectas}</div>`;
-                    let minutos = Math.floor((element.tiempo / 60000));
-                    let segundos = Math.floor((element.tiempo - minutos * 60000) / 1000);
-                    let milisegundos = element.tiempo - minutos * 60000 - segundos * 1000;
-                    minutos = minutos.toString().padStart(2, '0');
-                    segundos = segundos.toString().padStart(2, '0');
-                    milisegundos = milisegundos.toString().padStart(3, '0');
-                    mensajeTiempo += `<div>${minutos}' ${segundos}.${milisegundos}"</div>`;
-                });
-            }
-        datosPosicion.innerHTML = mensajePosicion;
-        datosNombre.innerHTML = mensajeNombre;
-        datosCorrectas.innerHTML = mensajeCorrectas;
-        datosTiempo.innerHTML = mensajeTiempo;
-    } catch (error) {
-        console.error('Error al cargar los datos:', error);
     }
+    datosPosicion.innerHTML = mensajePosicion;
+    datosNombre.innerHTML = mensajeNombre;
+    datosCorrectas.innerHTML = mensajeCorrectas;
+    datosTiempo.innerHTML = mensajeTiempo;
 }
 
 // Función para guardar datos en el servidor
 
-async function guardarDatos(data) {
+async function guardarDatos(dataJson) {
 
     // Ordena los datos antes de guardarlos
 
-    data.record.sort((a, b) => {
+    dataJson.record.sort((a, b) => {
         if (b.respuestasCorrectas != a.respuestasCorrectas) {
             return b.respuestasCorrectas - a.respuestasCorrectas;
         } else {
@@ -117,21 +118,18 @@ async function guardarDatos(data) {
 
     // Asigna el valor de la posición después de ordenar
 
-    data.record.forEach((item, index) => {
+    dataJson.record.forEach((item, index) => {
         item.posicion = index + 1;
     });
-    console.log(data.record);
 
     try {
-        const API_URL = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-        const API_KEY = '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK';
-        const response = await fetch(API_URL, {
+        const response = await fetch(apiUrl, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
-                'X-Master-Key': API_KEY
+                'X-Master-Key': apiKey
             },
-            body: JSON.stringify(data.record)
+            body: JSON.stringify(dataJson.record)
         });
 
         if (!response.ok) {
@@ -154,51 +152,41 @@ async function modificarRegistro() {
     let minutos = 0;
     let segundos = 0;
     ID = prompt("Ingresar la posición del jugador:");
-    try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-        const response = await fetch(apiUrl, {
-            headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Error al obtener los datos');
-        }
-        const data = await response.json();
-        jugador = data.record[ID - 1];
 
-        confirmar = confirm("¿modificar datos de " + jugador.nombre + " en posición " + jugador.posicion + "?");
+    await apiLeerRegistros();
+
+    jugador = dataJson.record[ID - 1];
+    confirmar = confirm("¿modificar datos de " + jugador.nombre + " en posición " + jugador.posicion + "?");
+    if (confirmar) {
+        confirmar = false;
+        confirmar = confirm("¿modificar nombre de " + jugador.nombre + "?");
         if (confirmar) {
-            confirmar = false;
-            confirmar = confirm("¿modificar nombre de " + jugador.nombre + "?");
-            if (confirmar) {
-                nombre = prompt("Ingresar nuevo nombre: ");
-                data.record[ID - 1].nombre = nombre;
-            }
-            confirmar = false;
-            confirmar = confirm("¿modificar cantidad de correctas? el dato actual es " + jugador.respuestasCorrectas);
-            if (confirmar) {
-                respuestasCorrectas = prompt("Ingresar nueva cantidad de correctas: ");
-                data.record[ID - 1].respuestasCorrectas = respuestasCorrectas;
-            }
-            confirmar = false;
-            confirmar = confirm("¿modificar tiempo? el dato actual es " + jugador.tiempo + "ms");
-            if (confirmar) {
-                minutos = parseInt(prompt("Ingresar minutos: "));
-                segundos = parseFloat(prompt("Ingresar segundos: "));
-                segundos = Number(segundos.toFixed(3));
-                tiempo = minutos * 60000 + segundos * 1000;
-                data.record[ID - 1].tiempo = tiempo;
-            }
-
-            guardarDatos(data);
-            //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
-            setTimeout(() => {
-                cargarRegistros()
-            }, 500);
+            nombre = prompt("Ingresar nuevo nombre: ");
+            dataJson.record[ID - 1].nombre = nombre;
         }
-    } catch (error) {
-        console.error('Error al cargar los datos:', error);
+        confirmar = false;
+        confirmar = confirm("¿modificar cantidad de correctas? el dato actual es " + jugador.respuestasCorrectas);
+        if (confirmar) {
+            respuestasCorrectas = prompt("Ingresar nueva cantidad de correctas: ");
+            dataJson.record[ID - 1].respuestasCorrectas = respuestasCorrectas;
+        }
+        confirmar = false;
+        confirmar = confirm("¿modificar tiempo? el dato actual es " + jugador.tiempo + "ms");
+        if (confirmar) {
+            minutos = parseInt(prompt("Ingresar minutos: "));
+            segundos = parseFloat(prompt("Ingresar segundos: "));
+            segundos = Number(segundos.toFixed(3));
+            tiempo = minutos * 60000 + segundos * 1000;
+            dataJson.record[ID - 1].tiempo = tiempo;
+        }
+
+        guardarDatos(dataJson);
+
+        //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
+
+        setTimeout(() => {
+            cargarRegistros()
+        }, 500);
     }
 }
 
@@ -206,65 +194,44 @@ async function borrarDatos() {
     let confirmacion = false;
     confirmacion = confirm("Se borrarán todos los datos, estás seguro?");
     if (confirmacion) {
-        try {
-            const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-            const response = await fetch(apiUrl, {
-                headers: {
-                    'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
-                }
-            });
-            if (!response.ok) {
-                throw new Error('Error al obtener los datos');
-            }
-            const data = await response.json();
 
-            if(data.record.length > 1) data.record.splice(0, data.record.length-1);
-            data.record[0].nombre = "...";
-            data.record[0].respuestasCorrectas = 0;
-            data.record[0].tiempo = 0;
+        await apiLeerRegistros();
 
-            console.log(data.record)
+        //Se borran todos los registros menos uno, al que se le asignan valores ficticios
 
-            guardarDatos(data);
+        if (dataJson.record.length > 1) dataJson.record.splice(0, dataJson.record.length - 1);
+        dataJson.record[0].nombre = "...";
+        dataJson.record[0].respuestasCorrectas = 0;
+        dataJson.record[0].tiempo = 0;
 
-            //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
-            setTimeout(() => {
-                cargarRegistros()
-            }, 500);
-        } catch (error) {
-            console.error('Error al cargar los datos:', error);
-        }
+        guardarDatos(dataJson);
+
+        //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
+
+        setTimeout(() => {
+            cargarRegistros()
+        }, 500);
+
     }
 }
 
 
 async function actualizarRanking(jugador) {
 
-    //--- recupera los datos guardados en data.json
+    //Recupera los datos guardados en dataJson.json
 
-    try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-        const response = await fetch(apiUrl, {
-            headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Error al obtener los datos');
-        }
-        const data = await response.json();
+    await apiLeerRegistros();
 
-        if((data.record.length == 1) && (data.record[0].nombre === "...")){
-            data.record[0].nombre = jugador.nombre;
-            data.record[0].respuestasCorrectas = jugador.respuestasCorrectas;
-            data.record[0].tiempo = jugador.tiempo;
-        }
-        else data.record.push(jugador);
+    //Si lo que existe es el registro vacío, se reemplazan los valores del mismo con los ingresados
 
-        guardarDatos(data);
-    } catch (error) {
-        console.error('Error al cargar los datos:', error);
-    }
+    if ((dataJson.record.length == 1) && (dataJson.record[0].nombre === "...")) {
+        dataJson.record[0].nombre = jugador.nombre;
+        dataJson.record[0].respuestasCorrectas = jugador.respuestasCorrectas;
+        dataJson.record[0].tiempo = jugador.tiempo;
+    } else dataJson.record.push(jugador);
+
+    guardarDatos(dataJson);
+
 }
 
 async function borrarRegistro() {
@@ -275,34 +242,22 @@ async function borrarRegistro() {
     let tiempo = 0;
     ID = prompt("Ingresar la posición del jugador: ");
 
-    try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
-        const response = await fetch(apiUrl, {
-            headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
-            }
-        });
-        if (!response.ok) {
-            throw new Error('Error al obtener los datos');
-        }
-        const data = await response.json();
-        data.record[ID - 1];
-        confirmar = confirm("¿eliminar datos de " + data.record[ID - 1].nombre + " en posición " + data.record[ID - 1].posicion + "?")
-        if (confirmar){
-            if(data.record.length == 1){
-                data.record[0].nombre = "...";
-                data.record[0].respuestasCorrectas = 0;
-                data.record[0].tiempo = 0;
-            }
-            else data.record.splice(ID - 1, 1);
-        }
-        guardarDatos(data);
+    await apiLeerRegistros();
 
-        //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
-        setTimeout(() => {
-            cargarRegistros()
-        }, 500);
-    } catch (error) {
-        console.error('Error al cargar los datos:', error);
+    dataJson.record[ID - 1];
+    confirmar = confirm("¿eliminar datos de " + dataJson.record[ID - 1].nombre + " en posición " + dataJson.record[ID - 1].posicion + "?")
+    if (confirmar) {
+        if (dataJson.record.length == 1) {
+            dataJson.record[0].nombre = "...";
+            dataJson.record[0].respuestasCorrectas = 0;
+            dataJson.record[0].tiempo = 0;
+        } else dataJson.record.splice(ID - 1, 1);
     }
+    guardarDatos(dataJson);
+
+    //Se agrega un pequeño retraso para asegurarnos de que se accedan a los datos ya modificados
+
+    setTimeout(() => {
+        cargarRegistros()
+    }, 500);
 }
--- JavaScript/ingresarIG.js
+++ JavaScript/ingresarIG.js
@@ -2,53 +2,59 @@ let iGExistente = false;
 let ranking = [];
 let nombreJugadorValido = false;
 let nombreJugadorepetido = false;
+const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
+const apiKey = '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK';
+
+// Valida que el texto ingresado sea una expresión válida
 
 function validarIG(nombreJugador) {
-
-    // Expresión regular para validar el nombre de usuario de Instagram
-
     const condiciones = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
     return condiciones.test(nombreJugador);
 }
 
+//Carga los datos del archivo JSON
+
 async function cargar() {
     try {
-        const apiUrl = 'https://api.jsonbin.io/v3/b/667b265fad19ca34f87ebdc4';
         const response = await fetch(apiUrl, {
             headers: {
-                'X-Master-Key': '$2a$10$WT5AxshcjZm2aoyff10BreyngNCLn3nPRaP4KL07tdYZ0.Z.ZO3NK' // Tu clave X-Master-Key aquí
+                'X-Master-Key': apiKey
             }
         });
         if (!response.ok) {
             throw new Error('Error al obtener los datos');
         }
         const data = await response.json();
-
         ranking = data.record;
-
     } catch (error) {
         console.error('Error al cargar los datos:', error);
     }
 }
 
+//Verifica que el texto ingresado no esté repetido y sea una expresión válida
+
 async function tomarIG() {
-    await cargar();
     let nombreJugador = document.getElementById('ingresarIG').value
     nombreJugador = nombreJugador.toLowerCase();
-    nombreJugadorValido = validarIG(nombreJugador);
-    nombreJugadorepetido = false;
+    if (validarIG(nombreJugador)) {
 
-    ranking.forEach(elemento => {
-        if (elemento.nombre === nombreJugador) {
-            nombreJugadorepetido = true;
-        }
-    })
+        await cargar();
 
-    if ((nombreJugadorValido == true) && (nombreJugadorepetido == false)) {
-        window.open("./pages/trivia.html", "_blank");
-        localStorage.setItem("nombreJugador", nombreJugador);
-    } else {
-        if (nombreJugadorValido == false) alert("Instagram inválido, volvé a ingresar");
-        if (nombreJugadorepetido == true) alert("Esta cuenta ya figura en nuestro ranking");
-    }
+        ranking.forEach(elemento => {
+            if (elemento.nombre === nombreJugador) nombreJugadorepetido = true;
+        })
+        if (nombreJugadorepetido == false) {
+            window.open("./pages/trivia.html", "_blank");
+            localStorage.setItem("nombreJugador", nombreJugador);
+        } else alert("Esta cuenta ya figura en nuestro ranking");
+    } else alert("Instagram inválido, volvé a ingresar");
 }
+
+//Elimina espacios en blanco
+
+ingresarIG.addEventListener('keyup', (e) => {
+    let texto = e.target.value;
+    e.target.value = texto.trim();
+})
+
+btnInicio.addEventListener('click', tomarIG);
